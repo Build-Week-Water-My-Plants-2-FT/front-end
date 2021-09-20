3bc6fd1945e1fdcabdf9c2b2a6384c8a0d4eef09
Arison13 <arisonarias@gmail.com>
2021-09-20 17:39:52 -0400

    got basic form values and able to see changes on the console,

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,19 +1,38 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import {Route, Link} from 'react-router-dom'
 import Login from './Components/Login/Login.js'
 import Header from './Components/Header/Header';
 
+
+//Initial States //
+  ///////////////////
+  const initialFormValues = {
+    username: '',
+    phoneNumber: ''
+    
+  }
+
+
 function App() {
-  
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const change = (name, value) => {
+    setFormValues({...formValues, [name]: value 
+    })
+  }
   return (
     <div className="App">
 
       <Header/>
       <h1> HELLO </h1>
       <Route path="/login">
-        <Login/>
-      </Route>
+        <Login 
+        values={formValues}
+        change={change}
+         />
+      </Route> 
 
     </div>
   );
