904ffd6e939f307906b0867ba1d8fbbcbca53f79
Arison13 <arisonarias@gmail.com>
2021-09-20 16:13:19 -0400

    got header working and looking ok, setput basic routes

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Route, Link} from 'react-router-dom'
+import Login from './Components/Login/Login.js'
+import Header from './Components/Header/Header';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header/>
+      <h1> HELLO </h1>
+      <Route path="/login">
+        <Login/>
+      </Route>
+
     </div>
   );
 }
---
+++ water-my-plants/src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link } from "react-router-dom";
+import './Header.css'
+function Header () {
+
+    return (
+      <div className="navbar"> 
+        
+        <nav>
+            <Link to="/"> Home </Link>
+            <Link to="/login"> Log In </Link>
+            <Link to="/signup"> Sign Up </Link>
+        </nav>
+      </div>
+    )
+}
+
+export default Header
