132587ed02cddfb8faf9ca8c86405bcf2c7b74ba
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-09-20 15:43:28 -0700

    signup form created

---
+++ water-my-plants/src/Forms/Signup.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// const initialValues = {
+//   username: "",
+//   phoneNumber: "",
+//   password: "",
+// };
+
+export default function Signup() {
+  return (
+    <div>
+      <input type="text" value="" placeholder="Username" />
+      <input type="number" value="" placeholder="Phone Number" />
+      <input type="password" value="" placeholder="Password" />
+    </div>
+  );
+}
