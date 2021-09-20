15a491ce9e573395eb57e988689ab5d5ced40ed8
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-09-20 16:29:04 -0700

    rearranged the signon page

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,38 +1,33 @@
 import "./App.css";
 import Signup from "./Forms/Signup";
-import React, {useState} from 'react';
-import {Route, Link} from 'react-router-dom'
-import Login from './Components/Login/Login.js'
-import Header from './Components/Header/Header';
-
+import React, { useState } from "react";
+import { Route, Link } from "react-router-dom";
+import Login from "./Forms/Login.js";
+import Header from "./Components/Header/Header";
 
 //Initial States //
-  ///////////////////
-  const initialFormValues = {
-    username: '',
-    phoneNumber: ''
-    
-  }
+///////////////////
+const initialFormValues = {
+  username: "",
+  phoneNumber: "",
+};
 
 function App() {
-
-  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formValues, setFormValues] = useState(initialFormValues);
 
   const change = (name, value) => {
-    setFormValues({...formValues, [name]: value 
-    })
-  }
+    setFormValues({ ...formValues, [name]: value });
+  };
   return (
     <div className="App">
-      <Signup />
-      <Header/>
+      <Header />
       <h1> HELLO </h1>
       <Route path="/login">
-        <Login 
-        values={formValues}
-        change={change}
-         />
-      </Route> 
+        <Login values={formValues} change={change} />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
     </div>
   );
 }
